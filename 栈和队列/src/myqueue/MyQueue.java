996f8fc4ee4561c63edd470f3ebcadd228c3078f
ji-cc <jicc0610@163.com>
2020-05-07 23:23:05 +0800

    提交代码

---
+++ 栈和队列/src/myqueue/MyQueue.java
@@ -0,0 +1,21 @@
+package myqueue;
+
+import mystack.LinkedStack;
+
+public class MyQueue {
+    public static void main(String[] args) {
+        LinkedQueue linkedQueue = new LinkedQueue();
+        linkedQueue.offer(1);
+        linkedQueue.offer(2);
+        linkedQueue.offer(3);
+        System.out.println(linkedQueue.peek());
+        System.out.println(linkedQueue.poll());
+
+        SequeueQueue sequeueQueue = new SequeueQueue();
+        sequeueQueue.offer(1);
+        sequeueQueue.offer(2);
+        sequeueQueue.offer(3);
+        System.out.println(sequeueQueue.peek());
+        System.out.println(sequeueQueue.poll());
+    }
+}
