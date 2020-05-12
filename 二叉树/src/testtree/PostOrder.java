76082e75c1897b206a816a37ff782634b6f244e4
ji-cc <jicc0610@163.com>
2020-05-12 19:40:48 +0800

    后序遍历二叉树（打印）

---
+++ 二叉树/src/testtree/PostOrder.java
@@ -0,0 +1,18 @@
+package testtree;
+
+public class PostOrder {
+    public static BuildTree.Node root = BuildTree.build();
+    //后序遍历  根的左子树-->根的右子树-->根节点
+    public static void postOrder(BuildTree.Node root) {
+        //如果是空树，不需要进行任何操作
+        if (root == null) {
+            return;
+        }
+        //递归处理左子树
+        postOrder(root.left);
+        //递归处理右子树
+        postOrder(root.right);
+        //访问根节点
+        System.out.print(root.val + " ");
+    }
+}
