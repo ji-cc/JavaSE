b0709a1f5bd3f03d33044ff6d8a28727c3f8292a
ji-cc <jicc0610@163.com>
2020-05-12 21:49:54 +0800

    给定一个二叉树，求二叉树中的结点个数 整个树的结点个数=根节点个数+ 左子树节点个数+右子树节点个数

--- 二叉树/src/testtree/Size.java
+++ 二叉树/src/testtree/Size.java
@@ -1,16 +1,16 @@
 package testtree;
 //给定一个二叉树，求二叉树中的结点个数
 public class Size {
-    public static BuildTree.Node root = BuildTree.build();
+
     public static int size(BuildTree.Node root) {
         if (root == null) {
             return 0; //空树里有0个结点
         }
         /*      A
               /   \
-             B      C
-           /   \      \
-          D     E      F
+             B     C
+           /   \     \
+          D     E     F
                /
               G                        */
         //整个树的结点个数=根节点个数          +          左子树节点个数                 +                      右子树节点个数
